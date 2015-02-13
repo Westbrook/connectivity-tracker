1db71e773b6c4eb14a199ff7c35991b7cd541320
Westbrook Johnson <westbrook.johnson@gmail.com>
2015-02-13 12:55:26 -0500

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,32 @@
 self.oninstall = function(event) {
   event.waitUntil(
-    caches.open('ct-v1').then(function(cache) {
+    caches.open('ct-v2').then(function(cache) {
       return cache.addAll([
         '/connectivity-tracker/src/'
       ]);
+    })
+  );
+};
+
+var expectedCaches = [
+  'ct-v2'
+];
+
+self.onactivate = function(event) {
+  // remove caches beginning "ct-" that aren't in
+  // expectedCaches
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (!/^ct-/.test(cacheName)) {
+            return;
+          }
+          if (expectedCaches.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 };
